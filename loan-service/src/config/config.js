86cc9ab03812c950ebfba0632e2d1076bd370a97
Rienzy06 <rienzyrifaldhy98.com>
2026-09-26 19:35:09 +0800

    chore: remove legacy service directories

--- loan-service/src/config/config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  PORT: process.env.PORT || 3002,
-  BOOK_SERVICE_URL: process.env.BOOK_SERVICE_URL || 'http://localhost:3001',
-  LOANS_FILE_PATH: path.join(__dirname, '../../data/loans.json')
-};
